f89311d09fa25b9b56a066af6cdf6e39f302decd
Leonid_Sanov <sanov.87@mail.ru>
2022-08-09 22:11:37 +0500

    hw02 complete

--- homework01/hw01/Program.cs
+++ homework01/hw01/Program.cs
@@ -4,8 +4,8 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
-int a = 5;
-int b = 7;
+int a = - 9;
+int b = - 3;
 
 if(a > b)
 {
--- homework01/hw02/Program.cs
+++ homework01/hw02/Program.cs
@@ -1,4 +1,37 @@
-﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и
+// выдаёт максимальное из этих чисел.
 // 2, 3, 7 -> 7
 // 44 5 78 -> 78
 // 22 3 9 -> 22
+
+int a = 2;
+int b = 3;
+int c = 7;
+
+int maximum = a;
+
+if (maximum > b)
+{
+    maximum = a;
+    if (maximum > c)
+    {
+        maximum = a;
+    }
+    else
+    {
+        maximum = c;
+    }
+}
+else
+{
+    maximum = b;
+    if (maximum > c)
+    {
+        maximum = b;
+    }
+    else
+    {
+        maximum = c;
+    }
+}
+Console.WriteLine(maximum);
