d78e7f7f2db564e08712991842b56094ecdf2b54
Leonid_Sanov <sanov.87@mail.ru>
2022-08-10 22:27:17 +0500

    lecture02_07-08-2022

---
+++ lecture02_07-08-2022/example04_ArrayLibrary/Program.cs
@@ -0,0 +1,53 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = 0;
+
+    while (index < count)
+    {
+
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10]; // создать новый массив, в котором будет 10 элементов
+
+FillArray(array); // этот метод создал массив
+array[4] = 4;
+array[6] = 4;
+
+
+PrintArray(array); // этот метод его распечатал
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
